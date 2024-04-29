385177c458a55d253029b1a04dbaee9978f0f77e
ekko1500 <ekko0499@gmail.com>
2024-04-29 15:59:50 +0630

    for app

---
+++ android/app/src/main/java/com/ekko/myapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ekko.myapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ekko.myapp',
+  appName: 'arduino-ui',
+  webDir: 'dist'
+};
+
+export default config;
